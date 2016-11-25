b44ded4895897154726b52619fb69638fa09c164
GeBlondeau <HE201360@students.ephec.be>
2016-11-25 09:56:46 +0100

    Add files via upload

---
+++ Code/CaseParkingGratuis.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package projetJAVA;
+
+/**
+ * @author Gérault
+ *
+ */
+public class CaseParkingGratuis extends Case {
+
+	
+	
+	public void recevoirArgent(){
+		Etudiant.ects = Etudiant.ects + 5;
+	}
+}
