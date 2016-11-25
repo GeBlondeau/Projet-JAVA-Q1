66c2ac79b08fe93945031dfe7b16246ee98023ae
GeBlondeau <HE201360@students.ephec.be>
2016-11-25 12:31:30 +0100

    Update Jeux.java

--- Code/Jeux.java
+++ Code/Jeux.java
@@ -4,27 +4,26 @@
 package projetJAVA;
 
 /**
- * @author Gérault
+ * @author GÃ©rault
  *
  */
 public class Jeux {
 	static int position;
-}
-/**
-public int lanceDes(){
-Random rand = new Random();
-int nombre1 = 1 + rand.nextInt(7-1); //Entre 1 et 6 
-int nombre2 = 1 + rand.nextInt(7-1); //Entre 1 et 6
-int nombreFinal = nombre1 + nombre2; //Additionner les deux dés	 
-return nombreFinal; //remplacer emplacement par la valeur de placeActeul à la fin du tour
-}
 
-// Modifier l'etat prison
-public void prison(){
-if (etatPrison == 0) // si été prison égal à zero alors jouer
-	lanceDes();
-else
-	etatPrison --; //sinon désincrementer l'etat de 1
+
+	public int lanceDes(){
+		Random rand = new Random();
+		int nombre1 = 1 + rand.nextInt(7-1); //Entre 1 et 6 
+		int nombre2 = 1 + rand.nextInt(7-1); //Entre 1 et 6
+		int nombreFinal = nombre1 + nombre2; //Additionner les deux dÃ©s	 
+		return nombreFinal; //remplacer emplacement par la valeur de placeActeul Ã  la fin du tour
+	}
+	public static void arrylist(String[] args) {
+		  listeJoueur = new ArrayList<String>();
+		  listeJoueur = null;					//Vider la liste
+		  listeJoueur.add("Etudiant1");			//Jouter des Ã©tudiants
+		  listeJoueur.add("Etudiant2");
+		  listeJoueur.add("Etudiant3");
+		  listeJoueur.add("Etudiant4");
+	}
 }
-}
-**/
