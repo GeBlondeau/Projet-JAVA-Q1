dfe880e4748b5158ecc5207415bb02910f45fec6
N-Lambin <he201386@students.ephec.be>
2016-11-27 17:57:02 +0100

    Add files via upload

--- Code/Plateau.java
+++ Code/Plateau.java
@@ -1,12 +1,21 @@
-/**
- * 
- */
-package projetJAVA;
+package projet_java;
 
-/**
- * @author Gérault
- *
- */
 public class Plateau {
+	
+	// chaque ligne est composée de : nomCase, achetable, gainECTS, prixAchat, nomMéthode
+	String tableauDeCase[][] = new String [40][4];
+	
+	// crée le plateau de jeu en définissant les "valeurs" de toutes les cases
+	public void lancementJeu(){
+		tableauDeCase[0][0] = ("Case départ");
+		tableauDeCase[0][1] = ("false");
+		tableauDeCase[0][2] = ("");
+		tableauDeCase[0][3] = ("");
+		tableauDeCase[0][4] = ("");
+	}
+
+	public void main() {
+
+	}
 
 }
